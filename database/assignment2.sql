caa0de1dbb9599b544095123cf56c549b3dc7b18
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-09-27 04:40:43 +0300

    COMMIT

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,2 +1,13 @@
 INSERT INTO account (first_name, last_name, email, password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE account
+SET account_type = 'Admin'
+WHERE email = 'tony@starkent.com';
+
+DELETE FROM account
+WHERE email = 'tony@starkent.com';
+
+UPDATE vehicle
+SET description = REPLACE(description, 'small interiors', 'a huge interior')
+WHERE vehicle_name = 'GM Hummer';
