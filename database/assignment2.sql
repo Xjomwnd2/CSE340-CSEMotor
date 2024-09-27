721c4bd44308c1356ccc1fe6e7a4f35d63da3c20
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-09-27 04:44:07 +0300

    COMMIT

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -11,3 +11,13 @@ WHERE email = 'tony@starkent.com';
 UPDATE vehicle
 SET description = REPLACE(description, 'small interiors', 'a huge interior')
 WHERE vehicle_name = 'GM Hummer';
+
+SELECT i.make, i.model, c.classification_name
+FROM inventory i
+INNER JOIN classification c
+ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
