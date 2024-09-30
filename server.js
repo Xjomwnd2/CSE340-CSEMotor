dd3a7e03ecb1cf056de91eb2a81a716dee24e8fd
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-09-30 05:00:23 +0300

    commit

--- server.js
+++ server.js
@@ -90,6 +90,22 @@ app.use(async (req, res, next) => {
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
