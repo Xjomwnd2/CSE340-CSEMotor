6838149c59522baf34090230a9819cf0a2aec8df
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-09-30 03:21:38 +0300

    commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = new express.Router(); 
 const invController = require("../controllers/invController");
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", invController.buildByClassificationId)
 
 module.exports = router;
