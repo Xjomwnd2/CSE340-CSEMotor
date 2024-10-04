b69bd6b92cff3ceb9f2421d6869017033cf73582
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-05 02:54:28 +0300

    COMMIT

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,22 +1,24 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
-const invCont = {}
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
+const invCont = {};
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
+  const classification_id = req.params.classificationId;
+  const data = await invModel.getInventoryByClassificationId(classification_id);
+  const grid = await utilities.buildClassificationGrid(data);
+  let nav = await utilities.getNav();
+  const className = data[0].classification_name;
+
+  // Render the view and pass the nav variable along with others
   res.render("./inventory/classification", {
     title: className + " vehicles",
-    nav,
+    nav, // Pass the nav variable here
     grid,
-  })
-}
+  });
+};
 
 // Move module.exports outside of the function
-module.exports = invCont
+module.exports = invCont;
