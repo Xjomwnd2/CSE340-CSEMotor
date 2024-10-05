55ba3b398e3bed8744645ef4bb3b0e437e9b9170
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-05 05:32:12 +0300

    COMMIT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
@@ -9,14 +10,15 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
+
 // Route and controller imports
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
-const inventoryRoute = require("./routes/inventoryRoute") // Added require statement for inventoryRoute
+const inventoryRoute = require("./routes/inventoryRoute"); // Added require statement for inventoryRoute
 const utilities = require('./utilities/index'); // Utilities functions
 
-
 app.use('/api', inventoryRoute);
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -28,10 +30,12 @@ app.set("layout", "./layouts/layout"); // Not at views root
  * Routes
  *************************/
 app.use(static);
+
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
 });
+
 // Inventory routes
 app.use("/inv", inventoryRoute); // Now properly linked
 
@@ -39,8 +43,8 @@ app.use("/inv", inventoryRoute); // Now properly linked
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Express Error Handler
@@ -60,40 +64,27 @@ app.use(async (err, req, res, next) => {
     res.status(500).send("An unexpected error occurred.");
   }
 });
-// FOR error.js in view folder
-// Other route handlers and middleware go above
+
 // Catch-all 404 handler for any routes that don't match
 app.use((req, res, next) => {
   res.status(404).render('errors/error', { title: 'Page Not Found', message: 'Sorry, the page you are looking for does not exist!' });
 });
+
 // Error-handling middleware for all other errors
 app.use((err, req, res, next) => {
   console.error(err.stack); // Log error stack trace to the console (for debugging)
   res.status(500).render('errors/error', { title: 'Something Went Wrong', message: err.message });
 });
+
 // Last route
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'});
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
 });
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message,
-    nav
-  });
-});
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on ${host}:${port}`);
 });
-
