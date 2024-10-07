18060858eaad494f4cb0bb02a653cfbc87badeab
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-07 22:47:29 +0300

    commit

--- controllers/SUV.js
+++
@@ -1,7 +0,0 @@
-// routes/suv.js
-const express = require('express');
-const router = express.Router();
-const suvController = require('../controllers/suvController'); // Import the controller
-// Route for getting SUV details
-router.get('/suv', suvController.getSuvDetails);
-module.exports = router;
