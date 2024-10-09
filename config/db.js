89206c059472ca1a0ff49fef72317242b02833eb
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-09 03:34:00 +0300

    commit

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
-// config/db.js
 const { Pool } = require('pg');
-require('dotenv').config(); // Load environment variables
+require('dotenv').config();
 
-// Create a new pool for connecting to PostgreSQL
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URLL=postgresql,//cse340:VvfsoVT9G06Yd240a3pWVLp71egvL4gk@dpg-crr1mnqj1k6c73e8526g-a.oregon-postgres.render.com/cse340_kql_z7do
-        rejectUnauthorized: false, // For hosted DB on Render
-    },
-);
+    connectionString: process.env.DATABASE_URL=postgresql,//cse340:VvfsoVT9G06Yd240a3pWVLp71egvL4gk@dpg-crr1mnqj1k6c73e8526g-a.oregon-postgres.render.com/cse340_kql_z7do// Load from .env file
+    ssl: {
+        rejectUnauthorized: false // Necessary for some remote PostgreSQL services like Render.com
+    }
+});
 
-module.exports = {
-    query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
