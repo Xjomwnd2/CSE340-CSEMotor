fc01c0e515e5c61a3bfa68eb77956fb00e9d52eb
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-09 21:25:24 +0300

    commit

--- controllers/navController.js
+++ controllers/navController.js
@@ -13,29 +13,3 @@ async function buildNavigation() {
 }
 
 module.exports = { buildNavigation };
-
-const buildByClassificationId = async (req, res) => {
-    try {
-      const classificationId = req.params.classificationId;
-      console.log("Classification ID:", classificationId);
-  
-      // Execute the query and get the data
-      const data = await inventoryModel.getInventoryByClassificationId(classificationId);
-  
-      // Check if the data is returned and if it's not empty
-      if (data && data.length > 0) {
-        // Proceed with the rest of your logic
-        const className = data[0].classification_name;
-        res.render("inventoryPage", { title: className, data });
-      } else {
-        // Handle the case when no data is returned
-        console.error("No data found for classification ID:", classificationId);
-        res.status(404).send("No inventory found for the selected classification.");
-      }
-    } catch (err) {
-      // Handle any unexpected errors
-      console.error("Error in buildByClassificationId:", err);
-      res.status(500).send("Server error");
-    }
-  };
-  
