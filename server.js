680b4dc1a715fcd397bdf7173b6fd28c95a1feda
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-09 20:03:03 +0300

    COMMIT

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const inventoryRoute = require("./routes/inventoryRoute"); // Ensure this is val
 const utilities = require('./utilities/index'); // Ensure this is valid
 app.use('/api', inventoryRoute);
 ///////////////Main Application Building Application/////////////
-// Mount static routes
+
+
+// Use the static route
 app.use('/static', staticRoutes);
 /////////////
 /* ***********************
