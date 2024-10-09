da8b6ebeab3765af41429ea70dd79d05c35abe29
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-09 04:10:26 +0300

    commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, postgresql://cse340:VvfsoVT9G06Yd240a3pWVLp71egvL4gk@dpg-crr1mnqj1k6c73e8526g-a.oregon-postgres.render.com/cse340_kql_z7do
-    ssl: {
+    ssl, {
         rejectUnauthorized: false // Important for services like Render.com
     }
 });
