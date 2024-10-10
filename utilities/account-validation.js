a97f968535cae396932abfd4177ef25d014448ea
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-11 01:52:52 +0300

    commit

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,3 +1,28 @@
 const utilities = require(".")
   const { body, validationResult } = require("express-validator")
   const validate = {}
+
+  /* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkRegData = async (req, res, next) => {
+    const { account_firstname, account_lastname, account_email } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      let nav = await utilities.getNav()
+      res.render("account/register", {
+        errors,
+        title: "Registration",
+        nav,
+        account_firstname,
+        account_lastname,
+        account_email,
+      })
+      return
+    }
+    next()
+  }
+  
+  module.exports = validate
+  
