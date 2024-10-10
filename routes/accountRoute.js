3bf33929aaeaa641e92c255483b9ce7a9c6cd4a8
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-11 01:25:30 +0300

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,12 +1,13 @@
 /* ****************************************
 *  Deliver login view
 * *************************************** */
+router.post('/register', utilities.handleErrors(accountController.registerAccount));
 async function buildLogin(req, res, next) {
     let nav = await utilities.getNav()
     res.render("account/login", {
       title: "Login",
       nav,
-    })
-  }
+    });
+  };
   
   module.exports = { buildLogin }
