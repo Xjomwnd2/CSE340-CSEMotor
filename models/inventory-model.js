ad8f501c8858bcdfac8d586087cbd3d35b2b941a
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-10 08:38:46 +0300

    commit

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,11 +1,13 @@
-const pool = require("../database/");
+const pool = require("../database/")
 
 /* ***************************
  *  Get all classification data
- *************************** */
-async function getClassifications() {
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
+ * ************************** */
+async function getClassifications(){
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
+
+module.exports = {getClassifications}
 
 /* ***************************
  *  Get all inventory items and classification_name by classification_id
