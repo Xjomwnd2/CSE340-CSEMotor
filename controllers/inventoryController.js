96524d43cb7bfc567a386cb8a864c42da6a9b11c
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-13 01:55:52 +0300

    commit

---
+++ controllers/inventoryController.js
@@ -0,0 +1,40 @@
+// In inventoryController.js
+const inventoryModel = require('../models/inventoryModel');
+const { validationResult } = require('express-validator');
+
+// Function to render the add-classification form
+exports.renderAddClassification = (req, res) => {
+  res.render('inventory/add-classification', {
+    title: 'Add New Classification',
+    errors: null,
+    message: req.flash('message'),
+  });
+};
+
+// Function to handle classification form submission
+exports.addClassification = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render('inventory/add-classification', {
+      title: 'Add New Classification',
+      errors: errors.array(),
+      message: null,
+    });
+  }
+
+  try {
+    const result = await inventoryModel.insertClassification(req.body.classificationName);
+
+    // If insertion is successful, show success message and refresh navigation
+    req.flash('message', 'Classification added successfully!');
+    res.redirect('/inventory/management'); // Adjust according to your management view
+
+  } catch (err) {
+    res.render('inventory/add-classification', {
+      title: 'Add New Classification',
+      errors: [{ msg: 'Error inserting classification into the database' }],
+      message: null,
+    });
+  }
+};
