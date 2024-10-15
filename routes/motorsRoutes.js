292121ca536d76d17e524caca1be4cae9f0361bb
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-15 05:21:53 +0300

    commit

--- routes/motorsRoutes.js
+++ routes/motorsRoutes.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const router = express.Router();
 
-// Define some example routes for motors
-router.get('/', (req, res) => {
-  res.render('motors', { title: 'Motors' });
-});
-
-// Export the router
-module.exports = router;
