f72c33906ea49d7d6183ee46589c23bd882d5aed
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-20 00:40:41 +0300

    commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,7 +1,7 @@
 const utilities = require('../utilities/');
 const baseController = {};
 
-req.flash("notice", "This is a flash message.");
+
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
   res.render("index", {title: "Home", nav});
