d76a0ae17e0a6ce169b18d59a73d47a1722122b6
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-20 00:22:37 +0300

    commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -10,9 +10,7 @@ baseController.buildHome = async function(req, res){
 module.exports = baseController
 
 // controllers/baseController.js
-
 exports.someFunction = (req, res) => {
-  // Use req.flash() correctly within the request handler
   req.flash("notice", "This is a flash message.");
-  res.redirect('/some-route'); // Redirect after setting the flash message
+  // other code...
 };
